8cd7682bdb5f366b715271e2ab2425261ae86b28
Alexandre Lerat <alexandre.lerat@gmail.com>
2018-04-27 03:25:45 +0200

    changement url pour auth0

--- src/client/environments/environment.prod.ts
+++ src/client/environments/environment.prod.ts
@@ -6,12 +6,12 @@ export const environment = {
       domain: 'oi-blagnac.eu.auth0.com',
       responseType: 'token id_token',
       audience: 'https://oi-blagnac.eu.auth0.com/userinfo',
-      redirectUri: 'https://objectif-image-blagnac.herokuapp.com',
+      redirectUri: 'https://oi-blagnac.herokuapp.com',
       scope: 'openid profile'
     },
     logout: {
       clientID: '8VEIyy6btqL2gX16I2IcOdo4OM5ArCii',
-      returnTo: 'https://objectif-image-blagnac.herokuapp.com'
+      returnTo: 'https://oi-blagnac.herokuapp.com'
     }
   }
 };
