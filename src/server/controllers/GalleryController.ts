cb2c66172939d8cae285f6cd5444b9b1835b7380
Alexandre Lerat <alexandre.lerat@gmail.com>
2018-06-07 19:51:01 +0200

    ALE - Mise en place API Flickr

--- src/server/controllers/GalleryController.ts
+++ src/server/controllers/GalleryController.ts
@@ -1,13 +1,10 @@
-import { Gallery, GalleryModel } from '../base/model/gallery';
-import { NextFunction, Request, Response, Router } from 'express';
-import { NotFoundHandler } from '../utils/404';
+import {Gallery} from '../base/model/gallery';
+import {Router} from 'express';
 import * as uuid from 'uuid';
-import * as mongoDB from '../base/BaseMongoDB';
-import { PhotoModel, Photo } from '../base/model/photo';
+import {Photo} from '../base/model/photo';
 import * as multer from 'multer';
 import * as fs from 'fs';
-import * as path from 'path';
-import { PhotoM } from '../base/model/photomodel';
+
 const uuidV4 = uuid.v4;
 const api = '/api/';
 const apiPrivate = api + 'private/';
@@ -15,122 +12,122 @@ const data = '../data/';
 
 const router: Router = Router();
 
-router.route('/').get(function(req, res) {
+router.route('/').get(function (req, res) {
   console.log('route /galleries/');
-  Gallery.find({}, function(err, gallery) {
-      if (err) {
-       res.send(err);
-      } else {
-        res.json(gallery);
-      }
-   });
+  Gallery.find({}, function (err, gallery) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(gallery);
+    }
+  });
 });
 router.route('/:numero')
-.get(function(req, res) {
-  console.log('get route /galleries/' + req.params.numero);
-  Gallery.find({'numero': req.params.numero}, function(err, galleries) {
+  .get(function (req, res) {
+    console.log('get route /galleries/' + req.params.numero);
+    Gallery.find({'numero': req.params.numero}, function (err, galleries) {
       if (err) {
-       res.send(err);
+        res.send(err);
       }
       if (galleries && galleries.length > 0) {
         res.json(galleries[0]);
       } else {
         res.json({});
       }
-   });
-}).post(function(req, res) {
+    });
+  }).post(function (req, res) {
   console.log('post route /galleries/' + req.params.numero);
   Gallery.createGallery(req, res);
 })
-.patch(function(req, res) {
-  console.log('patch route /galleries/' + req.params.numero);
-  Gallery.createGallery(req, res);
-});
+  .patch(function (req, res) {
+    console.log('patch route /galleries/' + req.params.numero);
+    Gallery.createGallery(req, res);
+  });
 
 
 router.route('/gallery/last')
-.get(function(req, res) {
-  console.log('post route /test/');
-  Gallery.findMaxNumber(function(err, galleries) {
+  .get(function (req, res) {
+    console.log('post route /test/');
+    Gallery.findMaxNumber(function (err, galleries) {
+      if (err) {
+        res.send(err);
+      }
+      if (galleries && galleries.length > 0) {
+        res.json(galleries[0]);
+      } else {
+        res.json({});
+      }
+    });
+
+  });
+router.route('/:numero/filterPhoto').get(function (req, res) {
+  console.log('route /galleries/' + req.params.numero);
+  Gallery.find({'numero': req.params.numero}, function (err, galleries) {
     if (err) {
-     res.send(err);
+      res.send(err);
     }
     if (galleries && galleries.length > 0) {
       res.json(galleries[0]);
     } else {
       res.json({});
     }
- });
-
-});
-router.route('/:numero/filterPhoto').get(function(req, res) {
-  console.log('route /galleries/' + req.params.numero);
-  Gallery.find({'numero': req.params.numero}, function(err, galleries) {
-      if (err) {
-       res.send(err);
-      }
-      if (galleries && galleries.length > 0) {
-        res.json(galleries[0]);
-      } else {
-        res.json({});
-      }
-   });
+  });
 });
 
-router.route('/types/:type').get(function(req, res) {
+router.route('/types/:type').get(function (req, res) {
   console.log('route /gallery/types/' + req.params.type);
-  Gallery.findByType(req.params.type, function(err, gallery) {
+  Gallery.findByType(req.params.type, function (err, gallery) {
     if (err) {
       console.log(err);
-     res.send(err);
+      res.send(err);
     } else {
       console.log('liste des galleries :' + gallery.length);
       res.json(gallery);
     }
- });
- // req.params.type;
+  });
+  // req.params.type;
 });
-router.route('/types/:type/annees').get(function(req, res) {
+router.route('/types/:type/annees').get(function (req, res) {
   console.log('route /gallery/types/' + req.params.type + '/annees');
-  Gallery.find({'type': req.params.type}, function(err, gallery) {
+  Gallery.find({'type': req.params.type}, function (err, gallery) {
     if (err) {
       console.error('err: ' + err);
-     res.send(err);
+      res.send(err);
     }
     let listAnnee = [];
     listAnnee = gallery.map(g => g.annee)
-          .filter((value, index, self) => self.indexOf(value) === index) ;
+      .filter((value, index, self) => self.indexOf(value) === index);
     res.json(listAnnee);
- });
- // req.params.type;
+  });
+  // req.params.type;
 });
-router.route('/types/:type/annees/:annee').get(function(req, res) {
+router.route('/types/:type/annees/:annee').get(function (req, res) {
   console.log('route /gallery/types/' + req.params.type + '/annees/' + req.params.annee);
-  Gallery.find({'type': req.params.type, 'annee': req.params.annee}, function(err, gallery) {
+  Gallery.find({'type': req.params.type, 'annee': req.params.annee}, function (err, gallery) {
     if (err) {
-     res.send(err);
+      res.send(err);
     }
     res.json(gallery);
   });
 });
 
 const storage = multer.diskStorage({
-   destination: function (req, file, cb) {
-    const chemin =  req.params.chemin;
+  destination: function (req, file, cb) {
+    const chemin = req.params.chemin;
     cb(null, chemin);
-    },
+  },
 
   filename: function (req, file, cb) {
-    Gallery.findOne({'numero': req.params.numero}, function(err, gallery) {
+    Gallery.findOne({'numero': req.params.numero}, function (err, gallery) {
       const p = new Photo();
       p.name = req.query.name;
       p.footer = req.query.footer;
       p.imageUrl = req.params.chemin + req.params.name;
       console.log(p);
 
-      gallery.photos.push( p);
-      console.log( gallery.photos);
-      gallery.save(function(err2) {
+      gallery.photos.push(p);
+      console.log(gallery.photos);
+      gallery.save(function (err2) {
         console.log('retour save');
         if (err) {
           console.log('retour save : ' + err);
@@ -143,14 +140,14 @@ const storage = multer.diskStorage({
 });
 
 
-const upload = multer({ storage: storage}).single('photo');
+const upload = multer({storage: storage}).single('photo');
 
-router.route('/:numero/photos/:name').post(validatePhoto, upload,  function(req, res) {
+router.route('/:numero/photos/:name').post(validatePhoto, upload, function (req, res) {
   res.send('image enregistrée');
 });
 
 function validatePhoto(req, res, next) {
-  Gallery.find({'numero': req.params.numero}, function(err, galleries) {
+  Gallery.find({'numero': req.params.numero}, function (err, galleries) {
     const errors = [];
     if (err) {
       console.log(err);
@@ -161,14 +158,14 @@ function validatePhoto(req, res, next) {
       console.log('nbPhoto : ' + nbPhoto);
       let nomPhoto = '';
       if (nbPhoto < 10) {
-        nomPhoto = '0' + nbPhoto  + '.jpg';
+        nomPhoto = '0' + nbPhoto + '.jpg';
       } else {
-        nomPhoto =  nbPhoto  + '.jpg';
+        nomPhoto = nbPhoto + '.jpg';
       }
       req.params.name = nomPhoto;
       let chemin = './assets/gallerie/';
       mkdir(chemin);
-      chemin = chemin + galleries[0].nom ;
+      chemin = chemin + galleries[0].nom;
       mkdir(chemin);
       chemin = chemin + '/' + galleries[0].annee + '/';
       mkdir(chemin);
@@ -177,18 +174,18 @@ function validatePhoto(req, res, next) {
       errors.push('pas de gallerie');
     }
     if (errors && errors.length > 0) {
-        const response = { errors: [] };
-        errors.forEach(function(err2) {
-          response.errors.push(err2.msg);
-        });
-        res.statusCode = 400;
-        return res.json(response);
+      const response = {errors: []};
+      errors.forEach(function (err2) {
+        response.errors.push(err2.msg);
+      });
+      res.statusCode = 400;
+      return res.json(response);
     }
     return next();
- });
- }
+  });
+}
 
- function mkdir(dirPath) {
+function mkdir(dirPath) {
   try {
     if (!fs.existsSync(dirPath)) {
       fs.mkdirSync(dirPath);
--- src/server/controllers/galleries.controller.ts
+++ src/server/controllers/galleries.controller.ts
@@ -3,10 +3,10 @@ import * as https from 'https';
 
 const router: Router = Router();
 const FLICKR_URL = 'https://api.flickr.com/services/rest/';
-const FLICKR_API_KEY = 'api_key=e3a7d42c3fe5af695954428715be897b';
+const FLICKR_API_KEY = 'api_key=3dea4e66f810d58490175a645e1fbbe5';
 const FLICKR_JSON_PARAMS = 'format=json&nojsoncallback=1';
-const FLICKR_USER_ID = 'user_id=129062663%40N03';
-const FLICKR_PHOTSET_ID = 'photoset_id=72157652411378405';
+const FLICKR_USER_ID = 'user_id=164591808%40N08';
+const FLICKR_PHOTSET_ID = 'photoset_id=72157694321614622';
 const FLICKR_GET_PHOTOS_METHOD = 'method=flickr.photosets.getPhotos';
 const FLCKR_GET_PHOTOS_EXTRAS = 'extras=url_m,url_o';
 const FLICK_GET_PHOTOS_URL = FLICKR_URL + '?' + [
@@ -17,28 +17,12 @@ const FLICK_GET_PHOTOS_URL = FLICKR_URL + '?' + [
   FLICKR_JSON_PARAMS,
   FLCKR_GET_PHOTOS_EXTRAS].join('&');
 
-const oauthToken = {
-  oauth_token: '72157694323426932-8cf561bb5d0cdfe3',
-  oauth_token_sig: 'ae07255749122b1e4606551b320c6baf'
-};
-
-https.get('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=27d344f68539283cb5262496457eebd7&photoset_id=72157694321614622&user_id=164591808%40N08&format=json&nojsoncallback=1&auth_token=72157694323426932-8cf561bb5d0cdfe3&api_sig=ae07255749122b1e4606551b320c6baf', resp2 => {
-  let data2: any = '';
-
-  resp2.on('data', (chunk) => {
-    data2 += chunk;
-  });
-
-  resp2.on('end', () => {
-    data2 = JSON.parse(data2);
-  });
-});
-
 /**
  * Récupération d'un ensemble de photos depuis un album Flickr.
  * On récupère l'url de les images miniature et originale
  */
 router.route('').get((req: Request, res: Response) => {
+  console.log(FLICK_GET_PHOTOS_URL);
   https.get(FLICK_GET_PHOTOS_URL, resp => {
     let data: any = '';
 
